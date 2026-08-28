d446d7554ced6204bb45efa48ef1254b9aa4f6a1
utehy-val-admin <vallab.utehy@gmail.com>
2026-08-28 13:50:29 +0700

    Update val.py

--- val.py
+++ val.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
-model = YOLO('/home/share/YOLOv12-H/runs/detect/yolov12n_egsa_v3-9/weights/best.pt')
-model.val(data='/home/share/yolov7/HAZE_dataset_3level25.8_real/3level_haze_gen.yaml', save_json=True, split='test')
+model = YOLO('best.pt')
+model.val(data='3level_haze_gen.yaml', save_json=True, split='test')
 
 #from ultralytics import YOLO
 #from ultralytics.utils.torch_utils import get_flops
-#model = YOLO('/home/share/YOLOv12-H/runs/detect/yolov12n_egsa_v3-10/weights/best.pt')
+#model = YOLO('best.pt')
 #print(get_flops(model.model, imgsz=640))
